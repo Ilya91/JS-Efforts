ca6d8a390b1fc10bb31ea3ce83d31b3f433f1463
Mironchikov Ilya <webmaster.1991@mail.ru>
2017-08-02 17:01:08 +0300

    add simple redux flow

---
+++ myreact/src/components/Counter.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import Article from './Article/index'
+import PropTypes from 'prop-types'
+import accordion from '../decorators/accordion'
+
+class ArticleList extends Component{
+
+    static propTypes = {
+        articles: PropTypes.array,
+        toggleOpenArticle: PropTypes.func.isRequired,
+        openArticleId: PropTypes.any
+    }
+
+    render() {
+        const { articles, openArticleId, toggleOpenArticle } = this.props
+        return(
+                <ul>
+                    {
+                        articles.map((article, i) =>
+                            <Article
+                                    key={article.id}
+                                    title={article.title}
+                                    date={article.date}
+                                    comments={article.comments}
+                                    isOpen = { article.id === openArticleId}
+                                    toggleOpen = { toggleOpenArticle(article.id) }
+                            >
+                                {article.text}
+                            </Article>
+                        )
+                    }
+                </ul>
+        )
+    }
+}
+export default accordion(ArticleList)
