7e873107ba34c4d3cbd0381c1c9c2299e0ccad84
Mironchikov Ilya <webmaster.1991@mail.ru>
2017-08-03 16:54:33 +0300

    init

---
+++ management/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <p>hello1</p>
+            </div>
+        )
+    }
+}
+export default App
