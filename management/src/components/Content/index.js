61560eccd9479dcf9bef75b55ce60d4d3ca48cc5
Mironchikov Ilya <webmaster.1991@mail.ru>
2017-08-07 18:15:04 +0300

    styling main page

--- management/src/components/Content/index.js
+++ management/src/components/Content/index.js
@@ -7,207 +7,41 @@ class Content extends Component {
             <div className="content-wrapper">
                 <section className="content">
                     <div className="row">
-                        <section className="col-lg-6">
+                        <section className="col-lg-8">
                             <div className="box box-primary">
                                 <div className="box-header">
                                     <i className="ion ion-clipboard"></i>
-                                    <h3 className="box-title">To Do List</h3>
-                                    <div className="box-tools pull-right">
-                                        <ul className="pagination pagination-sm inline">
-                                            <li><a href="#">&laquo;</a></li>
-                                            <li><a href="#">1</a></li>
-                                            <li><a href="#">2</a></li>
-                                            <li><a href="#">3</a></li>
-                                            <li><a href="#">&raquo;</a></li>
-                                        </ul>
-                                    </div>
+                                    <h3 className="box-title">НА СЕГОДНЯ</h3>
+                                    <span className="myWorkData"> Окт 9</span>
+                                    <span className="label label-info pull-right">0</span>
                                 </div>
                                 <div className="box-body">
-                                    <ul className="todo-list">
-                                        <li>
-                                            <span className="handle">
-                        <i className="fa fa-ellipsis-v"></i>
-                        <i className="fa fa-ellipsis-v"></i>
-                      </span>
-                                            <input type="checkbox" value=""/>
-                                                <span className="text">Design a nice theme</span>
-                                                <small className="label label-danger"><i className="fa fa-clock-o"></i> 2 mins</small>
-                                                <div className="tools">
-                                                    <i className="fa fa-edit"></i>
-                                                    <i className="fa fa-trash-o"></i>
-                                                </div>
-                                        </li>
-                                        <li>
-                      <span className="handle">
-                        <i className="fa fa-ellipsis-v"></i>
-                        <i className="fa fa-ellipsis-v"></i>
-                      </span>
-                                            <input type="checkbox" value=""/>
-                                                <span className="text">Make the theme responsive</span>
-                                                <small className="label label-info"><i className="fa fa-clock-o"></i> 4 hours</small>
-                                                <div className="tools">
-                                                    <i className="fa fa-edit"></i>
-                                                    <i className="fa fa-trash-o"></i>
-                                                </div>
-                                        </li>
-                                        <li>
-                      <span className="handle">
-                        <i className="fa fa-ellipsis-v"></i>
-                        <i className="fa fa-ellipsis-v"></i>
-                      </span>
-                                            <input type="checkbox" value=""/>
-                                                <span className="text">Let theme shine like a star</span>
-                                                <small className="label label-warning"><i className="fa fa-clock-o"></i> 1 day</small>
-                                                <div className="tools">
-                                                    <i className="fa fa-edit"></i>
-                                                    <i className="fa fa-trash-o"></i>
-                                                </div>
-                                        </li>
-                                        <li>
-                      <span className="handle">
-                        <i className="fa fa-ellipsis-v"></i>
-                        <i className="fa fa-ellipsis-v"></i>
-                      </span>
-                                            <input type="checkbox" value=""/>
-                                                <span className="text">Let theme shine like a star</span>
-                                                <small className="label label-success"><i className="fa fa-clock-o"></i> 3 days</small>
-                                                <div className="tools">
-                                                    <i className="fa fa-edit"></i>
-                                                    <i className="fa fa-trash-o"></i>
-                                                </div>
-                                        </li>
-                                        <li>
-                      <span className="handle">
-                        <i className="fa fa-ellipsis-v"></i>
-                        <i className="fa fa-ellipsis-v"></i>
-                      </span>
-                                            <input type="checkbox" value=""/>
-                                                <span className="text">Check your messages and notifications</span>
-                                                <small className="label label-primary"><i className="fa fa-clock-o"></i> 1 week</small>
-                                                <div className="tools">
-                                                    <i className="fa fa-edit"></i>
-                                                    <i className="fa fa-trash-o"></i>
-                                                </div>
-                                        </li>
-                                        <li>
-                      <span className="handle">
-                        <i className="fa fa-ellipsis-v"></i>
-                        <i className="fa fa-ellipsis-v"></i>
-                      </span>
-                                            <input type="checkbox" value=""/>
-                                                <span className="text">Let theme shine like a star</span>
-                                                <small className="label label-default"><i className="fa fa-clock-o"></i> 1 month</small>
-                                                <div className="tools">
-                                                    <i className="fa fa-edit"></i>
-                                                    <i className="fa fa-trash-o"></i>
-                                                </div>
-                                        </li>
-                                    </ul>
+                                    <div className="box-footer clearfix no-border">
+                                        <button type="button" className="btn btn-default pull-left"><i className="fa fa-plus"></i> Новая задача</button>
+                                    </div>
                                 </div>
-                                <div className="box-footer clearfix no-border">
-                                    <button type="button" className="btn btn-default pull-right"><i className="fa fa-plus"></i> Add item</button>
-                                </div>
+
                             </div>
                         </section>
-                        <section className="col-lg-6">
+                        <section className="col-lg-4">
                             <div className="box box-primary">
                                 <div className="box-header">
                                     <i className="ion ion-clipboard"></i>
-                                    <h3 className="box-title">To Do List</h3>
-                                    <div className="box-tools pull-right">
-                                        <ul className="pagination pagination-sm inline">
-                                            <li><a href="#">&laquo;</a></li>
-                                            <li><a href="#">1</a></li>
-                                            <li><a href="#">2</a></li>
-                                            <li><a href="#">3</a></li>
-                                            <li><a href="#">&raquo;</a></li>
-                                        </ul>
-                                    </div>
+                                    <h3 className="box-title">НА ЭТУ НЕДЕЛЮ</h3>
+                                    <span className="myWorkData"> Окт 9</span>
+                                    <span className="label label-info pull-right">0</span>
                                 </div>
-                                <div className="box-body">
-                                    <ul className="todo-list">
-                                        <li>
-                                            <span className="handle">
-                        <i className="fa fa-ellipsis-v"></i>
-                        <i className="fa fa-ellipsis-v"></i>
-                      </span>
-                                            <input type="checkbox" value=""/>
-                                                <span className="text">Design a nice theme</span>
-                                                <small className="label label-danger"><i className="fa fa-clock-o"></i> 2 mins</small>
-                                                <div className="tools">
-                                                    <i className="fa fa-edit"></i>
-                                                    <i className="fa fa-trash-o"></i>
-                                                </div>
-                                        </li>
-                                        <li>
-                      <span className="handle">
-                        <i className="fa fa-ellipsis-v"></i>
-                        <i className="fa fa-ellipsis-v"></i>
-                      </span>
-                                            <input type="checkbox" value=""/>
-                                                <span className="text">Make the theme responsive</span>
-                                                <small className="label label-info"><i className="fa fa-clock-o"></i> 4 hours</small>
-                                                <div className="tools">
-                                                    <i className="fa fa-edit"></i>
-                                                    <i className="fa fa-trash-o"></i>
-                                                </div>
-                                        </li>
-                                        <li>
-                      <span className="handle">
-                        <i className="fa fa-ellipsis-v"></i>
-                        <i className="fa fa-ellipsis-v"></i>
-                      </span>
-                                            <input type="checkbox" value=""/>
-                                                <span className="text">Let theme shine like a star</span>
-                                                <small className="label label-warning"><i className="fa fa-clock-o"></i> 1 day</small>
-                                                <div className="tools">
-                                                    <i className="fa fa-edit"></i>
-                                                    <i className="fa fa-trash-o"></i>
-                                                </div>
-                                        </li>
-                                        <li>
-                      <span className="handle">
-                        <i className="fa fa-ellipsis-v"></i>
-                        <i className="fa fa-ellipsis-v"></i>
-                      </span>
-                                            <input type="checkbox" value=""/>
-                                                <span className="text">Let theme shine like a star</span>
-                                                <small className="label label-success"><i className="fa fa-clock-o"></i> 3 days</small>
-                                                <div className="tools">
-                                                    <i className="fa fa-edit"></i>
-                                                    <i className="fa fa-trash-o"></i>
-                                                </div>
-                                        </li>
-                                        <li>
-                      <span className="handle">
-                        <i className="fa fa-ellipsis-v"></i>
-                        <i className="fa fa-ellipsis-v"></i>
-                      </span>
-                                            <input type="checkbox" value=""/>
-                                                <span className="text">Check your messages and notifications</span>
-                                                <small className="label label-primary"><i className="fa fa-clock-o"></i> 1 week</small>
-                                                <div className="tools">
-                                                    <i className="fa fa-edit"></i>
-                                                    <i className="fa fa-trash-o"></i>
-                                                </div>
-                                        </li>
-                                        <li>
-                      <span className="handle">
-                        <i className="fa fa-ellipsis-v"></i>
-                        <i className="fa fa-ellipsis-v"></i>
-                      </span>
-                                            <input type="checkbox" value=""/>
-                                                <span className="text">Let theme shine like a star</span>
-                                                <small className="label label-default"><i className="fa fa-clock-o"></i> 1 month</small>
-                                                <div className="tools">
-                                                    <i className="fa fa-edit"></i>
-                                                    <i className="fa fa-trash-o"></i>
-                                                </div>
-                                        </li>
-                                    </ul>
+                                <div className="box-header">
+                                    <i className="ion ion-clipboard"></i>
+                                    <h3 className="box-title">НА СЛЕД. НЕДЕЛЮ</h3>
+                                    <span className="myWorkData"> Окт 9</span>
+                                    <span className="label label-info pull-right">0</span>
                                 </div>
-                                <div className="box-footer clearfix no-border">
-                                    <button type="button" className="btn btn-default pull-right"><i className="fa fa-plus"></i> Add item</button>
+                                <div className="box-header">
+                                    <i className="ion ion-clipboard"></i>
+                                    <h3 className="box-title">ПОЗЖЕ</h3>
+                                    <span className="myWorkData"> Окт 9</span>
+                                    <span className="label label-info pull-right">0</span>
                                 </div>
                             </div>
                         </section>
